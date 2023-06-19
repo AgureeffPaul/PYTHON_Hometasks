8f41a0812706fd42251811e204fee61892348d00
PAUL AGUREEFF <p.agureev@mail.ru>
2023-06-20 02:16:09 +0300

    Залил семинар 2

--- HOME_Training/LESSON2/Task_2_1.py
+++ HOME_Training/LESSON2/Task_2_1.py
@@ -3,19 +3,32 @@
 # чтобы все монетки были повернуты вверх одной и той же стороной. Выведите минимальное 
 # количество монет, которые нужно перевернуть. Количество монет и их положение (0 или 1) 
 # пользователь вводит с клавиатуры.
-n = int(input("Введите количество монет: "))
-coins = list(map(int, input("Введите состояние каждой монеты (0 - решка, 1 - герб) через пробел: ").split()))
-heads_count = sum(coins)
-flip_count = 0
-if heads_count >= n - heads_count:
-    for i in range(n):
-        if coins[i] == 0:
-            coins[i] = 1
-            flip_count += 1
-else:
-    for i in range(n):
-        if coins[i] == 1:
-            coins[i] = 0
-            flip_count += 1
 
-print("Минимальное количество переворотов: ", flip_count)
+# ВАРИАНТ 1
+
+# n = int(input("Введите количество монет: "))
+# coins = list(map(int, input("Введите состояние каждой монеты (0 - решка, 1 - герб) через пробел: ").split()))
+# heads_count = sum(coins)
+# flip_count = 0
+# if heads_count >= n - heads_count:
+#     for i in range(n):
+#         if coins[i] == 0:
+#             coins[i] = 1
+#             flip_count += 1
+# else:
+#     for i in range(n):
+#         if coins[i] == 1:
+#             coins[i] = 0
+#             flip_count += 1
+
+# print("Минимальное количество переворотов: ", flip_count)
+
+# ВАРИАНТ 2
+
+num = int(input('Введите количество монет: '))
+heads = 0 # орел 1
+tails = 0 # решка 0
+for i in range(num):
+        current_coin_status = int(input(f'Введите положение монеты {i} 0 или 1:  '))
+        if current_coin_status == 0: tails += 1
+        if current_coin_status == 1: heads += 1
